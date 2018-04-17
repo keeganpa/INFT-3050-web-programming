70c72f5546c5951fde420aad21519170dac26b13
jimmyb63 <c3215105@uon.edu.au>
2018-04-17 17:01:47 +1000

    admin class

---
+++ Japallum_Apparel/UL/Classes/Admin.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UL.Classes
+{
+    public class Admin
+    {
+        private int iD;
+        private String firstName;
+        private String lastName;
+        private String emailAddress;
+        private String password;
+        private Boolean active;
+
+        public Boolean Active
+        {
+            get
+            {
+                return active;
+            }
+
+            set
+            {
+                active = value;
+            }
+        }
+        public int ID
+        {
+            get
+            {
+                return iD;
+            }
+
+            set
+            {
+                iD = value;
+            }
+        }
+
+        public String fName
+        {
+            get
+            {
+                return firstName;
+            }
+
+            set
+            {
+                firstName = value;
+            }
+        }
+
+        public String lName
+        {
+            get
+            {
+                return lastName;
+            }
+
+            set
+            {
+                lastName = value;
+            }
+        }
+
+        public String eAdd
+        {
+            get
+            {
+                return emailAddress;
+            }
+
+            set
+            {
+                emailAddress = value;
+            }
+        }
+
+        public String Password
+        {
+            get
+            {
+                return password;
+            }
+
+            set
+            {
+                password = value;
+            }
+        }
+
+        public Admin(int id, String fN, String lN, String email, String pass)
+        {
+            iD = id;
+            firstName = fN;
+            lastName = lN;
+            emailAddress = email;
+            password = pass;
+            active = true;
+        }
+    }
+}
